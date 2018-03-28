a941370e71ef990dc0b1259dd91d40a110e371df
Emily Everett <emilyeverett@Emilys-MacBook-Pro.local>
2018-03-28 13:32:44 -0400

    added Parser class and updated documentation on processor

---
+++ Parser.h
@@ -0,0 +1,66 @@
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+#include <string>
+#include <unordered_map>
+
+#include "Instruction.h"
+#include "RegisterFile.h"
+
+//////////////////////////////////////////////////////////////////////////////////////////
+/// @brief Class to parse inputs
+/// @TODO Decide if methods should be private
+
+class Parser {
+  private:
+    std::string inputFileName;                ///< name of input file
+    std::string mipsFileName;                 ///< name of mips file (from input file)
+    std::string registerFileName;             ///< name of register data file (from input file)
+    std::string memoryFileName;               ///< name of memory data file (from input file)
+    std::string outputMode;                   ///< either single-step or batch
+    bool debug;                               ///< whether to print debugging info
+    bool printMemory;                         ///< whether to print memory contents
+    bool writeToFile;                         ///< whether to write output to file
+    std::string outputFile;                   ///< name of file to write to
+
+  public:
+    //////////////////////////////////////////////////////////////////////////////////////
+    /// @brief Constructor
+    ///
+    /// @param file Name of input file (should come from command line)
+    Parser(std::string file) { inputFileName = file; }
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    /// @brief Deleted copy constructor
+    /// @param other Reference to object to be copied
+    Parser(const Parser& other) = delete;
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    /// @brief Default destructor
+    ~Parser() = default;
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    /// @brief Parses the input file using inputFileName to set names of other files
+    void parseInput() {}
+
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    /// @brief Parses the MIPS assembly code
+    /// @details Calls the Instruction constructor
+    /// @return unordered map of instructions
+    const std::unordered_map<std::string, Instruction>& parseMIPS() {}
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    /// @brief Parses the registerfile state
+    /// @details Calls the RegisterFile constructor
+    /// @return Reference to the RegisterFile object
+    const RegisterFile& parseRegisterFile() {}
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    /// @brief Parses the memory file
+    /// @return reference to unordered map of memory data <address, memory value> -- must be converted from hex to bin
+    const std::unordered_map<std::string, std::string>& parseMemory() {}
+
+};
+
+#endif
