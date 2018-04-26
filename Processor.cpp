f6cd6352cc4490a746e0d8e85fd7bd9a82ff4861
Nico Zingarelli <ndzingarelli@gmail.com>
2018-04-26 13:59:24 -0400

    commented out debug prints in processor.cpp

--- Processor.cpp
+++ Processor.cpp
@@ -63,9 +63,9 @@ void Processor::
 decode(){
   //Decode:
   //   a. Control gets Instruction. 
-  cout << "Decode 1" << endl;
+  // cout << "Decode 1" << endl;
   control.setControlLines(currentInstruction);
-  cout << "Decode 2" << endl;
+  // cout << "Decode 2" << endl;
   //   b. MUX1 gets RegDst and instruction 20-16 and 15-11
   // control.print();
 
@@ -76,22 +76,22 @@ decode(){
 
   // muxRegDist.print();
 
-  cout << "Decode 3" << endl;
+  // cout << "Decode 3" << endl;
   //   c. Register things: 
   //       - Register gets RegWrite
-  cout << "///////////////////////////////////////////////////" << control.getRegDst() << endl;
+  // cout << "///////////////////////////////////////////////////" << control.getRegDst() << endl;
   registerFile.setRegWrite(control.getRegWrite());
 
-  cout << "Decode 4" << endl;
+  // cout << "Decode 4" << endl;
   //       - Read Register 1 gets instruction 25-21
   registerFile.setReadReg1(stoi(bin2dec(currentInstruction.getBits(25, 21))));
   registerFile.setReadData1(registerContents.at(registerFile.getReadReg1()));
 
-  cout << "Decode 5" << endl;
+  // cout << "Decode 5" << endl;
   //       - Read Register 2 gets instruction 20-16
   registerFile.setReadReg2(stoi(bin2dec(currentInstruction.getBits(20, 16))));
   registerFile.setReadData2(registerContents.at(registerFile.getReadReg2()));
-  cout << "Decode 6" << endl;
+  // cout << "Decode 6" << endl;
   //       - Write Register gets MUX1 return thing.
 
     // cout << "REGISTER BEING SET: " << stoi(bin2dec(muxRegDist.getResult())) << endl << endl;
@@ -105,14 +105,14 @@ decode(){
 
     // registerFile.setWriteReg(stoi(bin2dec(muxRegDist.getResult())));
   
-  cout << "Decode 7" << endl;
+  // cout << "Decode 7" << endl;
 
   // registerFile.print();
   //   d. Sign extend instruction 15-0 and send it to shift left 2 while
   //      maintaining the number of bits. Send the result of this and 
   //      ALU1 to ALU2.
   signExtendedNum = signExt(currentInstruction.getBits(15, 0));
-  cout << "Decode 8" << endl;
+  // cout << "Decode 8" << endl;
   // cout << "alu 1 result: " << aluOne.getALUresult() << endl;
   // cout << "sl2 : " << shiftLeft2(signExtendedNum, true) << endl;
 
@@ -125,7 +125,7 @@ decode(){
 
   aluTwo.operate(addCode, aluOne.getALUresult(), bin2hex(shiftLeft2(signExtendedNum, true)));   
   // aluTwo.print();
-  cout << "Decode 9  Good through here" << endl;
+  // cout << "Decode 9  Good through here" << endl;
   // true = maintain number of bits. 
 }
 
@@ -137,7 +137,7 @@ execute(){
   //cout << "readData: " << registerFile.getReadData2() << endl;
   muxALUSrc.operate(control.getALUSrc(), bin2hex(signExtendedNum), registerFile.getReadData2());
   // muxALUSrc.print();
-  cout << "Execute 1" << endl;
+  // cout << "Execute 1" << endl;
 
   //   b. ALU3 gets ALU Control and Read Data 1 and result of MUX2
   // cout << "getaluop0: " << control.getALUOp0() << endl;
@@ -145,7 +145,7 @@ execute(){
   // cout << "Func field of current inst: " << currentInstruction.getFuncField() << endl;
   aluThree.setControl(control.getALUOp0(), control.getALUOp1(), currentInstruction.getFuncField());
   // aluThree.print(); 
-  cout << "Execute 2" << endl;  
+  // cout << "Execute 2" << endl;  
   // cout << "registerFile: " << registerFile.getReadData1() << endl;
   // cout << "Alu 3 control: " << aluThree.getControl() << endl; 
   // cout << "ALUControl from control: " << control.getALUControl() << endl;
@@ -154,11 +154,11 @@ execute(){
 
   // cout << "aluthree /get control " << aluThree.getControl() << endl;
   aluThree.operate(aluThree.getControl(), muxALUSrc.getResult(), registerFile.getReadData1());
-  aluThree.print();
-  cout << "ALUTHREE RESULT: " << aluThree.getALUresult() << endl;
+  // aluThree.print();
+  // cout << "ALUTHREE RESULT: " << aluThree.getALUresult() << endl;
   // cout << "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" << aluThree.getALUresult() << endl;
   // aluThree.print();
-  cout << "Execute 3" << endl;
+  // cout << "Execute 3" << endl;
   //   c. send the result of ALU1 and ALU2 and (branch AND zero from ALU3) 
   //      to MUX5.
   muxBranch.operate((control.getBranch()&&aluThree.getZero()), aluTwo.getALUresult(), aluOne.getALUresult());
@@ -166,7 +166,7 @@ execute(){
   //   d. Shift left 2 takes instruction 25-0 and shifts 2(increasing 2 bits)
   //      and concatenate those 28 bits with the bits from ALU1. Send this(jump
   //      address) and result of MUX5 to MUX4.
-  cout << "Execute 4" << endl;
+  // cout << "Execute 4" << endl;
 
   //  cout << "PC: " << programCounter.getPC() << endl;
   //  cout << "Current Instruction: " << currentInstruction.getBits(25, 0) << endl;
@@ -179,11 +179,11 @@ execute(){
   muxJump.operate(control.getJump(), uglyString, muxBranch.getResult());
   // muxJump.print();
   //   e. Send result of MUX4 to PC?
-  cout << "Execute 5" << endl;
+  // cout << "Execute 5" << endl;
   // cout << "Printing Program counter before incrementing" << programCounter.getPC() << endl;
   programCounter.setPC(muxJump.getResult());
   // cout << "Printing Program counter after incrementing" << programCounter.getPC() << endl;
-  cout << "Execute 6" << endl;  
+  // cout << "Execute 6" << endl;  
 }
 
 void Processor::
@@ -192,22 +192,22 @@ memory(){
   //   a. Data Memory gets MemWrite and MemRead and Result of ALU3(address)
   //      and read data2(write data).
   /// @TODO DATA MEMORY NEEDS TO BE DONE 
-  cout << "Memory 0" << endl;
+  // cout << "Memory 0" << endl;
 
   if (control.getMemWrite()) {
-    cout << "Memory 1" << endl;  
+    // cout << "Memory 1" << endl;  
     // do the things
     // cout << aluThree.getALUresult() << endl;
     // cout << bin2hex(aluThree.getALUresult()) << endl;
     MemoryData[aluThree.getALUresult()] = registerFile.getReadData2();
-    cout << "Memory 2" << endl;
+    // cout << "Memory 2" << endl;
   }
-  cout << "Memory 3" << endl;
+  // cout << "Memory 3" << endl;
 }
 
 void Processor::
 writeback(){
-  cout << "Writeback 0" << endl;
+  // cout << "Writeback 0" << endl;
   // Writeback:
   //   b. MUX3 gets MemtoReg and Read data(from data memory) and result of ALU3
   ///@TODO DO WHAT IS WRITTEN ABOVE  
@@ -219,46 +219,46 @@ writeback(){
   //
   //MemoryData.at(aluThree.getALUresult()) << endl;
   if(control.getRegWrite()) {
-    cout << "THIS IF STATEMENT IS SATAN" << endl;
-    cout << aluThree.getALUresult() << endl;
+    // cout << "THIS IF STATEMENT IS SATAN" << endl;
+    // cout << aluThree.getALUresult() << endl;
     // cout << InstructionData.at((aluThree.getALUresult())).getBinStr() << endl;
     if(control.getMemRead()){
       // cout << "Hello darkness my old friend" << endl;
       // cout << "clearly this is wtorng" << endl;
       // cout << MemoryData.at(aluThree.getALUresult()) << endl;
-      muxALUSrc.print();
+      // muxALUSrc.print();
 
-      cout << registerContents.at(2) << endl;
+      // cout << registerContents.at(2) << endl;
 
-      cout << signExtendedNum<< endl;
+      // cout << signExtendedNum<< endl;
       muxMemToReg.operate(control.getMemToReg(), MemoryData.at(aluThree.getALUresult()), aluThree.getALUresult());
     }
     else {
       muxMemToReg.operate(control.getMemToReg(), "the cake is a lie", aluThree.getALUresult());
     }
   
-    muxMemToReg.print();
+    // muxMemToReg.print();
   }
 
-  cout << "Writeback 1" << endl;
+  // cout << "Writeback 1" << endl;
 
 
   //   a. Write data in register file gets the result of MUX3.
   ///@TODO need to have functionality in registerFile to check boolean before 
   //       writing to the write data reg
   if(control.getRegWrite()){
-    if(registerFile.getWriteReg() == 3){
-      cout << "HOLY FUCK NUGGETS THIS IS WHERE SHIT GOES DOWN!!!!!" << endl;
-      cout << "\t\t RegisterFile Data: " << registerContents[registerFile.getWriteReg()] << endl;
-      cout << "\t\t What we are trying to put here: " << registerFile.getWriteData() << endl;
+    // if(registerFile.getWriteReg() == 3){
+    //   cout << "HOLY FUCK NUGGETS THIS IS WHERE SHIT GOES DOWN!!!!!" << endl;
+    //   cout << "\t\t RegisterFile Data: " << registerContents[registerFile.getWriteReg()] << endl;
+    //   cout << "\t\t What we are trying to put here: " << registerFile.getWriteData() << endl;
 
-    }
-    cout << "REGISTER NUMBER: " << registerFile.getWriteReg() << endl;
+    // }
+    // cout << "REGISTER NUMBER: " << registerFile.getWriteReg() << endl;
     // cout << "\t\t\tThis is what is wrong with life: ~setWrite - MUX RESULT = " << muxMemToReg.getResult() << endl;
     
     registerFile.setWriteData(muxMemToReg.getResult());
     registerContents[registerFile.getWriteReg()] = registerFile.getWriteData();
-    cout << "Writeback 2" << endl;
+    // cout << "Writeback 2" << endl;
   }
 }
 
